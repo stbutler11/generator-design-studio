5a45694c7092d22d42c9da3d08d7d2094d97c53f
alnmaher <maher.alan@sap.com>
2015-05-18 15:10:49 +0100

    generator-sdk

---
+++ generator-sdk/generators/app/index.js
@@ -0,0 +1,72 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var mkdirp = require('yeoman-generator/node_modules/mkdirp');
+
+module.exports = yeoman.generators.Base.extend({
+  
+  prompting: function () {
+    var done = this.async();
+
+    var prompts = [{
+      //type: 'confirm',
+      name: 'sdkName',
+      message: 'What would you like to name your SDK component?',
+      default: 'template'
+    },
+    {
+      //type: 'confirm',
+      name: 'bundleID',
+      message: 'PLease choose a bundle identifier',
+      default: 'com.sap.sample'
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+
+      // To access props later use this.props.someOption;
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      var bundleID = this.props.bundleID;
+      var sdkName = this.props.sdkName;
+      var sdkNameOneWord = sdkName.replace(/\s+/g, '');
+      var sdkNameLower = sdkNameOneWord.toLowerCase();
+        
+        
+      this.fs.copyTpl(
+        this.templatePath(),
+        this.destinationPath(),
+        {   
+            bundle: bundleID,
+            title: sdkName,
+            titleLower: sdkNameLower,
+            titleOneWord: sdkNameOneWord
+        }
+          
+      );
+    
+    //Needed otherwise .project file not copied
+      this.fs.copyTpl(
+        this.templatePath('src/component/.project'),
+        this.destinationPath('src/component/.project'),
+        {   
+            bundle: bundleID,
+            titleLower: sdkNameLower
+        }
+          
+      );
+        
+      mkdirp.sync(this.destinationPath('src/dist/'));
+    
+      this.bowerInstall();    
+        
+    }
+
+  }
+
+});
---
+++ generator-sdk/generators/app/templates/src/component/res/js/component.js
@@ -0,0 +1,20 @@
+sap.designstudio.sdk.Component.subclass("<%= bundle %>.<%= titleLower %>.<%= titleOneWord %>", function() {
+
+	var that = this;
+
+	this.init = function() {
+		this.$().addClass("coloredBox");
+		this.$().click(function() {
+			that.fireEvent("onclick");
+		});
+	};
+
+	this.color = function(value) {
+		if (value === undefined) {
+			return this.$().css("background-color");
+		} else {
+			this.$().css("background-color", value);
+			return this;
+		}
+	};
+});
