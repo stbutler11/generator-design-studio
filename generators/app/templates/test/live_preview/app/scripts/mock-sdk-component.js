a82a576ef975a041720090750225dac34a82b54f
Stuart Butler <stuart.butler01@sap.com>
2015-05-19 16:12:45 +0100

    Displaying updates to properties

--- generators/app/templates/test/live_preview/app/scripts/mock-sdk-component.js
+++ generators/app/templates/test/live_preview/app/scripts/mock-sdk-component.js
@@ -9,8 +9,27 @@ sap.designstudio.sdk.Component = {
 	subclass: function(name, mixinFn) {
 		'use strict';
 
-		var ctx = {},
-			properties;
+		var EVENT = 'event',
+			PROPERTIES_CHANGED = 'propertiesChanged',
+			ctx = {},
+			properties,
+			oldPropertyValues = {},
+			propertyChanged = function(name, value) {
+				var changed = false;
+				if (value && value !== oldPropertyValues[name]) {
+					changed = true;
+				} else if (!value && oldPropertyValues[name]) {
+					changed = true;
+				}
+				return changed;
+			},
+			updateActionTable = function(type, value) {
+				var timeCell = '<td>' + new Date().toLocaleTimeString() + '</td>',
+					typeName = type === EVENT ? 'Event' : 'Properties Changed',
+					typeCell = '<td>' + typeName + '</td>',
+					eventCell = '<td>' + value + '</td>';
+				$('#event-table').append('<tr class="' + type + '">' + timeCell + typeCell + eventCell + '</tr>');
+			};
 
 		ctx.afterUpdate = _.noop;
 		ctx.init = _.noop;
@@ -19,18 +38,25 @@ sap.designstudio.sdk.Component = {
 			return $('#component-div');
 		};
 
-		ctx.fireEvent = function() {
-			var timeCell = '<td>' + new Date().toLocaleTimeString() + '</td>',
-				eventCell = '<td>' + JSON.stringify(arguments) + '</td>';
-			$('#event-table').append('<tr>' + timeCell + eventCell + '</tr>');
+		ctx.$.browser = $.browser;
+
+		ctx.fireEvent = function(e) {
+			updateActionTable(EVENT, e);
 		};
 
-		ctx.firePropertiesChangedAndEvent = function() {
-			console.log('firePropertiesChangedAndEvent', arguments);
+		ctx.firePropertiesChanged = function(properties) {
+			updateActionTable(PROPERTIES_CHANGED, properties.join(','));
+		};
+
+		ctx.firePropertiesChangedAndEvent = function(properties, e) {
+			updateActionTable(PROPERTIES_CHANGED, properties.join(','));
+			updateActionTable(EVENT, e);
 		};
 
 		ctx.setProperty = function(name, value) {
-			ctx[name](value);
+			if (propertyChanged(name, value)) {
+				ctx[name](value);
+			}
 		};
 
 		ctx.getProperty = function(name) {
